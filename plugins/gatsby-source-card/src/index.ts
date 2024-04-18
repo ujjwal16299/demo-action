aeb964ce9ff4acd5839bfd4920d3131468eecc81
Ujjwal Pathania <99011264+ujjwal16299@users.noreply.github.com>
2024-04-18 23:37:01 +0530

    up

--- plugins/gatsby-source-card/src/index.ts
+++
@@ -1,4 +0,0 @@
-import createSchemaCustomization  from '../gatsby-node';
-import Card from '../src/pages/card'; // Assuming the path to your Card component is './card'
-
-export { createSchemaCustomization, Card };
